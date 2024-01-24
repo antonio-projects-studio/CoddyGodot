307bd5ac52f08a9edb97e1bb44ced13fa511a62f
Antonio <antonio.projects.studio@gmail.com>
2024-01-24 19:07:14 +0300

    some changes with character

--- Modules/Module-4-Godot/Roguelike/Characters/Character.gd
+++ Modules/Module-4-Godot/Roguelike/Characters/Character.gd
@@ -1,14 +1,14 @@
 extends KinematicBody2D
-class_name Character, 'res://Art/v1.1 dungeon crawler 16x16 pixel pack/heroes/knight/knight_idle_anim_f0.png'
-
-
+class_name Character, "res://Art/v1.1 dungeon crawler 16x16 pixel pack/heroes/knight/knight_idle_anim_f0.png"
 
 const FRICTION: float = 0.15
 
-export (int) var accerelation: int = 40
-export (int) var max_speed: int = 100
+export(int) var hp: int = 2
 
-export (bool) var flying: bool = false
+export(int) var accerelation: int = 40
+export(int) var max_speed: int = 100
+ 
+export(bool) var flying: bool = false
 
 onready var state_machine: Node = get_node("FiniteStateMachine")
 onready var animated_sprite: AnimatedSprite = get_node("AnimatedSprite")
@@ -20,9 +20,17 @@ var velocity: Vector2 = Vector2.ZERO
 func _physics_process(_delta: float) -> void:
 	velocity = move_and_slide(velocity)
 	velocity = lerp(velocity, Vector2.ZERO, FRICTION)
-
-
+	
+	
 func move() -> void:
 	mov_direction = mov_direction.normalized()
 	velocity += mov_direction * accerelation
-	velocity = velocity.clamped(max_speed)
+	velocity = velocity.limit_length(max_speed)
+
+func take_damage(dam: int, dir: Vector2, force: int) -> void:
+	hp -= dam
+	if hp > 0:
+		state_machine.set_state(state_machine.states.hurt)
+		velocity += dir * force 
+	else:
+		state_machine.set_state(state_machine.states.dead)
