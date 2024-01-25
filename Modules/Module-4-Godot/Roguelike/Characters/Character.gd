f1d793defa28ba4ecbba3e00135de5c4363b54e1
Antonio <antonio.projects.studio@gmail.com>
2024-01-25 18:14:39 +0300

    add HealtBar

--- Modules/Module-4-Godot/Roguelike/Characters/Character.gd
+++ Modules/Module-4-Godot/Roguelike/Characters/Character.gd
@@ -4,7 +4,9 @@ class_name Character, "res://Art/v1.1 dungeon crawler 16x16 pixel pack/heroes/kn
 
 const FRICTION: float = 0.15
 
-export (int) var hp: int = 2
+export(int) var max_hp: int = 2
+export(int) var hp: int = 2 setget set_hp
+signal hp_changed(new_hp)
 
 export (int) var accerelation: int = 40
 export (int) var max_speed: int = 100
@@ -30,10 +32,15 @@ func move() -> void:
 
 
 func take_damage(dam: int, dir: Vector2, force: int) -> void:
-	hp -= dam
+	self.hp -= dam
 	if hp > 0:
 		state_machine.set_state(state_machine.states.hurt)
 		velocity += dir * force
 	else:
 		state_machine.set_state(state_machine.states.dead)
 		velocity += dir * force * 2
+
+
+func set_hp(new_hp: int) -> void:
+	hp = int(clamp(new_hp, 0, max_hp))
+	emit_signal("hp_changed", hp)
