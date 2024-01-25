046dd6cc7af9851a5b606b706aaa359f69337ec4
Antonio <antonio.projects.studio@gmail.com>
2024-01-25 16:50:59 +0300

    add Player attack

--- Modules/Module-4-Godot/Roguelike/Characters/Character.gd
+++ Modules/Module-4-Godot/Roguelike/Characters/Character.gd
@@ -19,11 +19,21 @@ var velocity: Vector2 = Vector2.ZERO
 
 
 func _physics_process(_delta: float) -> void:
- velocity = move_and_slide(velocity)
- velocity = lerp(velocity, Vector2.ZERO, FRICTION)
+	velocity = move_and_slide(velocity)
+	velocity = lerp(velocity, Vector2.ZERO, FRICTION)
 
 
 func move() -> void:
- mov_direction = mov_direction.normalized()
- velocity += mov_direction * accerelation
- velocity = velocity.limit_length(max_speed)
+	mov_direction = mov_direction.normalized()
+	velocity += mov_direction * accerelation
+	velocity = velocity.limit_length(max_speed)
+
+
+func take_damage(dam: int, dir: Vector2, force: int) -> void:
+	hp -= dam
+	if hp > 0:
+		state_machine.set_state(state_machine.states.hurt)
+		velocity += dir * force
+	else:
+		state_machine.set_state(state_machine.states.dead)
+		velocity += dir * force * 2
