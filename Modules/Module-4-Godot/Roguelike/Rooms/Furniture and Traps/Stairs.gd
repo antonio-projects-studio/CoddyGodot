cbe623ead14c9322c7d555fb47ba24201b3dc9b5
Antonio <antonio.projects.studio@gmail.com>
2024-01-29 13:40:49 +0300

    deleted noused

--- Modules/Module-4-Godot/Roguelike/Rooms/Furniture and Traps/Stairs.gd
+++
@@ -1,7 +0,0 @@
-extends Area2D
-
-onready var collision_shape: CollisionShape2D = get_node("CollisionShape2D")
-
-func _on_Stairs_body_entered(_body: KinematicBody2D) -> void:
-	collision_shape.set_deferred("disabled", true)
-	SceneTransistor.start_transition_to("res://Game.tscn")
